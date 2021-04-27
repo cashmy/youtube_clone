703e9ffbf7f406523e67824e263ad4679f627bf9
cashmy <cmyers880@gmail.com>
2021-04-27 14:48:56 -0500

    Corrected bug in search_results_table

--- src/components/SearchResults/search_results_table.jsx
+++ src/components/SearchResults/search_results_table.jsx
@@ -4,69 +4,70 @@ import {
     MDBCard, 
     MDBCardBody,
     MDBTable,
+    MDBTableHead,
     MDBTableBody,
 } from "mdbreact";
-import axios from 'axios'
+// import axios from 'axios'
 
 const SearchResultsTable = (props) => {
 
-    const getVideos = () => {
-        let videoData = {}
+    // const getVideos = () => {
+    //     let videoData = {}
 
-        if (props.search_text === '' && props.related_yt_video_id !== '') {
-            // TODO: Replace with services call ??
-            axios.get('https://www.googleapis.com/youtube/v3/search?relatedToVideoId=' + props.related_yt_video_id +'&type=video&key=AIzaSyA655YtG7MH6YYIr3ZJPsA89s3GlRhLA4I')
-                .then(response => { videoData = response.data })
-                .catch(error => {alert('There was an error! ' + error.message)})
-        } else {
-            // TODO: Replace with services call ??
-            axios.get('https://www.googleapis.com/youtube/v3/search?q='+ props.search_text +'&key=AIzaSyA655YtG7MH6YYIr3ZJPsA89s3GlRhLA4I')
-                .then(response => { videoData = response.data })
-                .catch(error => {alert('There was an error! ' + error.message)})
-        }
-        return videoData
-    }
+    //     if (props.search_text === '' && props.related_yt_video_id !== '') {
+    //         // TODO: Replace with services call ??
+    //         axios.get('https://www.googleapis.com/youtube/v3/search?relatedToVideoId=' + props.related_yt_video_id +'&type=video&key=AIzaSyA655YtG7MH6YYIr3ZJPsA89s3GlRhLA4I')
+    //             .then(response => { videoData = response.data })
+    //             .catch(error => {alert('There was an error! ' + error.message)})
+    //     } else {
+    //         // TODO: Replace with services call ??
+    //         axios.get('https://www.googleapis.com/youtube/v3/search?q='+ props.search_text +'&key=AIzaSyA655YtG7MH6YYIr3ZJPsA89s3GlRhLA4I')
+    //             .then(response => { videoData = response.data })
+    //             .catch(error => {alert('There was an error! ' + error.message)})
+    //     }
+    //     return videoData
+    // }
 
     const handleOnClick = (video) => {
+        console.log("I have been clicked: ", video)
         // TODO: update props info to VideoPlayer component
     }
 
-    const mapVideoCards = (videoData) => {
-        let newVideosMapResult = videoData.map((video) => {
-            video.yt_video_id = ''
-            video.title = 'title'
-            video.description = 'desc'
+    const mapVideoCards = (videos) => {
+        let newVideosMapResult = props.videos.map((video) => {
             video.card = 
                 <MDBCard className="mb-3"
                          onClick={() => handleOnClick(video)}
                          style={{ cursor: 'pointer' }}
                 >
                     <MDBCardBody>
-                        Video Card data
+                        Video Card data:
+                        {video.yt_video_id}
+                        {video.title}
+                        {video.description}
                     </MDBCardBody>
                 </MDBCard>
             return video;
         });
+        console.log('Map results: ',newVideosMapResult)
         return newVideosMapResult
     }
 
     const data = {
-        // columns: [
-
-        //     {
-        //         label: 'Track',
-        //         field: 'track',
-        //         sort: 'asc',
-        //         width: 50
-        //     },
-        // ],
+        columns: [
+            {
+                label: 'Video List',
+                field: 'card'
+            },
+        ],
         rows: mapVideoCards()
     }
 
     return (
         <MDBCol className="col-md-3 mt-4">
             <MDBCard>
-                <MDBTable scrollY>
+                <MDBTable scrollY borderless='true' maxHeight='1200px'>
+                    <MDBTableHead columns={data.columns}/>
                     <MDBTableBody rows={data.rows} />
                 </MDBTable>
                 {/* <MDBCardBody>
@@ -101,6 +102,22 @@ const SearchResultsTable = (props) => {
 SearchResultsTable.defaultProps = {
     search_text : '',
     related_yt_video_id: 'V65uAHzofbg',
+    videos: [{
+        yt_video_id: '1'
+    },
+    {
+        yt_video_id: '2'
+    },
+    {
+        yt_video_id: '3'
+    },
+    {
+        yt_video_id: '4'
+    },
+    {
+        yt_video_id: '5'
+    },
+    ]
 }
 
 export default SearchResultsTable;
