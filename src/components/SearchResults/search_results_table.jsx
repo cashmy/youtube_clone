038397612b3b154c63e10e5c79b81d739a32de56
cashmy <cmyers880@gmail.com>
2021-04-28 13:36:22 -0500

    Temp deactivated YouTube API calls for testing

--- src/components/SearchResults/search_results_table.jsx
+++ src/components/SearchResults/search_results_table.jsx
@@ -14,25 +14,24 @@ const SearchResultsTable = (props) => {
     const [videoData, setVideoData] = useState({})
 
     useEffect(() => {
-            console.log('\n*** Use Effect ***')
-            console.log('Props.search: ', props.search_text)
-            console.log('Props.related ID: ', props.related_yt_video_id )
+        // Temporarily deactivate YouTube API requests and using props data ...
             if (props.search_text === '' && props.related_yt_video_id !== '') {
                 // TODO: Replace with services call ??
-                axios.get('https://www.googleapis.com/youtube/v3/search?relatedToVideoId=' + props.related_yt_video_id +'&type=video&key=AIzaSyA655YtG7MH6YYIr3ZJPsA89s3GlRhLA4I')
+                axios.get('https://www.googleapis.com/youtube/v3/search?relatedToVideoId=' + props.related_yt_video_id +'&type=video&key=AIzaSyDqq8qNVoNnuzEc3WA7KtosxpJbJpZMkN0&maxResults=10')
                     .then(response => { 
-                        console.log("Related - Response data: ", response.data)
-                        setVideoData(response.data) 
+                        console.log("Related - Response data: ", response.data.items)
+                        setVideoData(response.data.items) 
                     })
                     .catch(error => {alert('There was an error! ' + error.message)})
             } else {
                 // TODO: Replace with services call ??
-                axios.get('https://www.googleapis.com/youtube/v3/search?q='+ props.search_text +'&key=AIzaSyA655YtG7MH6YYIr3ZJPsA89s3GlRhLA4I')
+                axios.get('https://www.googleapis.com/youtube/v3/search?q='+ props.search_text +'&key=AIzaSyDqq8qNVoNnuzEc3WA7KtosxpJbJpZMkN0&maxResults=10')
                     .then(response => {
-                        console.log("Search - Response data: ", response.data)
-                         setVideoData(response.data) })
+                        console.log(">>> Search - Response data: ", response.data.items)
+                         setVideoData(response.data.items) })
                     .catch(error => {alert('There was an error! ' + error.message)})
             }
+            setVideoData(props.items)
             return 
     }, [props.search_text, props.related_yt_video_id])
 
@@ -44,8 +43,41 @@ const SearchResultsTable = (props) => {
     }
 
     const mapVideoCards = () => {
-        console.log("Video Data: ", videoData)
-        let newVideosMapResult = props.videos.map((video, yt_video_id) => {
+
+        console.log('\n*** Map Video Cards ***')
+        console.log('Props.search: ', props.search_text)
+        console.log('Props.related ID: ', props.related_yt_video_id )
+        console.log("\n\n *** Video Data: ", videoData)
+        console.log('typeof: ', typeof(videoData))
+
+        // Cannot use "videoData.map" as it is an object (checked using typeof function)
+        // So iterating over state variable and building my own array
+        let videosMapResult = []
+        for (let i = 0; i < videoData.length; i++) {
+
+            let card = <MDBCard className="mb-3"
+                                onClick={() => handleOnClick(card)}
+                                style={{ cursor: 'pointer' }}
+                        >
+                            <MDBCardBody>
+                                Video Card data:
+                                {videoData[i].id.videoId}
+                            </MDBCardBody>
+                        </MDBCard>
+
+            videosMapResult.push({
+                yt_video_id: videoData[i].id.videoId, 
+                card: card 
+                })
+         }
+
+
+
+        console.log('VideosMapResult: ', videosMapResult)
+        console.log('typeof videosMapResult: ', typeof(videosMapResult))
+
+        let newVideosMapResult = props.videos.map((video) => {
+            console.log("Video Record: ", video)
         //    video.key={yt_video_id}
             video.card = 
                 <MDBCard className="mb-3"
@@ -62,7 +94,7 @@ const SearchResultsTable = (props) => {
             return video;
         });
         console.log('Map results: ',newVideosMapResult)
-        return newVideosMapResult
+        return videosMapResult
     }
 
     const data = {
@@ -129,6 +161,48 @@ SearchResultsTable.defaultProps = {
     {
         yt_video_id: '5'
     },
+    ],
+    items: [
+        {
+            "kind": "youtube#searchResult",
+            "etag": "yx7a8-Nd08nUyfdfdD5hz8R8FIo",
+            "id": {
+                "kind": "youtube#video",
+                "videoId": "MArhfhV5Z8A"
+            }
+        },
+        {
+            "kind": "youtube#searchResult",
+            "etag": "b1H8Bnr226C4zW2fqtRMwPJjPh0",
+            "id": {
+                "kind": "youtube#video",
+                "videoId": "n7yvElqUUtQ"
+            }
+        },
+        {
+            "kind": "youtube#searchResult",
+            "etag": "U9eKJUwEhOIS2M2H_3NBH5ns-sM",
+            "id": {
+                "kind": "youtube#video",
+                "videoId": "DgW14g0chfU"
+            }
+        },
+        {
+            "kind": "youtube#searchResult",
+            "etag": "-W_n0Ey78KhyOmyOdRNy3S1RnpM",
+            "id": {
+                "kind": "youtube#video",
+                "videoId": "iC1K-G8m9PI"
+            }
+        },
+        {
+            "kind": "youtube#searchResult",
+            "etag": "GxG4Bm8rH-Fe-19tumzlMZ4nRRg",
+            "id": {
+                "kind": "youtube#video",
+                "videoId": "5jEWNUQzrXY"
+            }
+        }
     ]
 }
 
