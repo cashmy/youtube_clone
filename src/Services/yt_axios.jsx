81a71358b3164777655a2f85e52cc8a988512f1c
DemetriceW20 <demetrice_williams@hotmail.com>
2021-05-01 09:19:52 -0800

    Changed API key and saveComment function in add_edit_comment.jsx

--- src/Services/yt_axios.jsx
+++ src/Services/yt_axios.jsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const getVideoInfo = async (yt_video_id) => {
     let videoData = {}  
-    let response = await axios.get('https://www.googleapis.com/youtube/v3/videos?part=snippet&id='+ yt_video_id +'&key=AIzaSyDqq8qNVoNnuzEc3WA7KtosxpJbJpZMkN0')
+    let response = await axios.get('https://www.googleapis.com/youtube/v3/videos?part=snippet&id='+ yt_video_id +'&key=AIzaSyDhPzky3hbBomJ4zBtc4IrdqMF4emK3vX4')
             videoData.title = response.data.items[0].snippet.title
             videoData.description = response.data.items[0].snippet.description
     return videoData
